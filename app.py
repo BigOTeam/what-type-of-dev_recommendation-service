bb3ebab748e7fccf029dbe81379bf23317f1d2e1
person00333 <person00333@gmail.com>
2022-03-31 14:14:06 +0900

    Feat : surveys.result test 코드

--- app.py
+++ app.py
@@ -1,16 +1,26 @@
-from flask import Flask,request  # 서버 구현을 위한 Flask 객체 import
-from flask_restx import Api, Resource  # Api 구현을 위한 Api 객체 import
+from flask import Flask, request  # 서버 구현을 위한 Flask 객체 import
+from flask_restx import Api, Resource, reqparse, fields  # Api 구현을 위한 Api 객체 import
+from flask import json
 import pandas as pd
 import tensorflow as ts
+from job_class import job_data
+
+
 app = Flask(__name__)  # Flask 객체 선언, 파라미터로 어플리케이션 패키지의 이름을 넣어줌.
 api = Api(app)  # Flask 객체에 Api 객체 등록
 
 
-@api.route('/hello')  # 데코레이터 이용, '/hello' 경로에 클래스 등록
+@api.route('/surveys/results_test')  # 데코레이터 이용, '/hello' 경로에 클래스 등록
 class HelloWorld(Resource):
-    def get(self):  # GET 요청시 리턴 값에 해당 하는 dict를 JSON 형태로 반환
-        test = request.args.get('test')
-        return [{"hello": int(test)},{"hello": int(test)}]
+    def post(self):  # GET 요청시 리턴 값에 해당 하는 dict를 JSON 형태로 반환
+        rankData = []
+        rankData.append(job_data(1, 1, "Websites", "", "https://i.ibb.co/tLTkY32/Web.png").__dict__)
+        rankData.append(job_data(2, 2, "Utilities", "", "https://i.ibb.co/vZ9p437/Util.png").__dict__)
+        rankData.append(job_data(3, 6, "Finance", "", "https://i.ibb.co/SvD3jKG/Finance.png").__dict__)
+        j = job_data(3, 6, "Finance", "", "https://i.ibb.co/SvD3jKG/Finance.png")
+        print(j.__dict__)
+        return {"rankData": rankData}
+
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=8083)
---
+++ job_class.py
@@ -0,0 +1,7 @@
+class job_data:
+    def __init__(self, job_rank,job_id, job_name, job_description, job_img):
+        self.job_rank = job_rank
+        self.job_id = job_id
+        self.job_name = job_name
+        self.job_description = job_description
+        self.job_img = job_img
